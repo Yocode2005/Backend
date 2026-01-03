d3f1f263823732f7addc347afbfe7a69aec03787
Yocode2005 <yogeshchhandhwal2005@gmail.com>
2026-01-03 11:52:42 +0530

    add register controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,21 @@
 import {Router} from "express";
 import {registerUser} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
 const router = Router();
 
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([ // middleware to handle multiple file uploads just before the controller
+        {
+             name: 'avatar', 
+             maxCount: 1
+        },
+        { 
+            name: 'images', 
+            maxCount: 5 
+        }
+    ]),  
+    registerUser
+    )
 
 export default router;
